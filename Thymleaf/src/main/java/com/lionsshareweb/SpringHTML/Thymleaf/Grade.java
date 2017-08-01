b35f0764b82134fb077618e9d49f8438a9e57991
adrbin42 <adriennebing@me.com>
2017-08-01 16:56:09 -0400

    resubmit project

---
+++ Thymleaf/src/main/java/com/lionsshareweb/SpringHTML/Thymleaf/Grade.java
@@ -0,0 +1,7 @@
+package com.lionsshareweb.SpringHTML.Thymleaf;
+
+public enum Grade {
+    First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth;
+
+}
+
---
+++ Thymleaf/src/main/java/com/lionsshareweb/SpringHTML/Thymleaf/Student.java
@@ -0,0 +1,34 @@
+package com.lionsshareweb.SpringHTML.Thymleaf;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    Grade grade;
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
